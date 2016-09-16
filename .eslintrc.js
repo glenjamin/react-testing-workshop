307f69fbff518e8df2bfbb7a2380f03aa0997f0f
Glen Mailer <glenjamin@gmail.com>
2016-09-16 16:50:25 +0100

    More plumbing, attempt at simple webpack setup

---
+++ .eslintrc.js
@@ -0,0 +1,9 @@
+module.exports = {
+  "env": {
+    "node": true,
+    "es6": true
+  },
+  "parser": "babel-eslint",
+  "plugins": ["react"],
+  "extends": ["eslint:recommended", "plugin:react/recommended"]
+}
